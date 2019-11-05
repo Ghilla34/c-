7a0d9fb4c7756808074c81a264eef784fabcab81
Ghilla34 <vitor.vingador@hotmail.com>
2019-11-04 23:44:31 -0200

    talarico

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+         public static int soma(int b , int c)
+        {
+            return b + c;
+        }
+           
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
